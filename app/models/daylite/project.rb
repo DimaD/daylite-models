cf117c8f84ea671ccb7d27cbf0c75683cf0a8bd5
Dmitriy Dzema <dmitriy@dzema.name>
2009-02-09 00:36:54 +1000

    Add statusCodes strings representation

--- app/models/daylite/project.rb
+++ app/models/daylite/project.rb
@@ -1,5 +1,4 @@
 class Daylite::Project < Daylite::Base
-  #status_codes = { 1 => "", 2 => "", 3 => "", 4 => ""}
   belongs_to :owner, :class_name => "User", :foreign_key => "assignedToID"
 
   has_many :roles, :foreign_key => "projectID"
@@ -7,4 +6,19 @@ class Daylite::Project < Daylite::Base
 
   has_many :project_task_joins, :foreign_key => "projectID"
   has_many :tasks, :through => :project_task_joins
+
+  def status_codes
+    @@status_codes ||= {
+      0 => "In Progress",
+      1 => "Deffered",
+      2 => "Canceled",
+      3 => "Abandoned",
+      4 => "New",
+      7 => "Done",
+    }
+  end
+
+  def status_string
+    status_codes[attributes["statusCode"]] || "Unknown status"
+  end
 end
