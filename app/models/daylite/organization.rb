a19be37ed67df4b88b05af89c7800f11ab0e25d3
Dmitriy Dzema <dmitriy@dzema.name>
2009-02-08 15:34:04 +1000

    add User model add organization method to Task model which select first organization from corresponding association

--- app/models/daylite/organization.rb
+++ app/models/daylite/organization.rb
@@ -2,6 +2,8 @@ class Daylite::Organization < Daylite::Base
   set_primary_key "_rowid"
   set_table_name "Organization"
 
+  belongs_to :owner, :class_name => "User", :foreign_key => "assignedToID"
+
   has_many :organization_task_joins, :foreign_key => "organizationID"
   has_many :tasks, :through => :organization_task_joins
 end
--- app/models/daylite/task.rb
+++ app/models/daylite/task.rb
@@ -1,6 +1,16 @@
 class Daylite::Task < Daylite::Base
   set_table_name "Task"
 
+  belongs_to :owner, :class_name => "User", :foreign_key => "assignedToID"
+
   has_many :organization_task_joins, :foreign_key => "taskID"
   has_many :organizations, :through => :organization_task_joins
+
+  def organization
+    organizations[0]
+  end
+
+  def to_s
+    "#<Daylite::Task:#{title}>"
+  end
 end
