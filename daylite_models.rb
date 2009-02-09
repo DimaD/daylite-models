c48d803c21d2ef2aaf14a596f8dd4dc9c6d8a889
Dmitriy Dzema <dmitriy@dzema.name>
2009-02-09 20:41:22 +1000

    move database.yml to config subdirectory

--- daylite_models.rb
+++ daylite_models.rb
@@ -1,8 +1,13 @@
 $: << File.join(File.dirname(__FILE__), 'lib')
 require 'init'
 
-yaml = YAML::load(File.read('database.yml'))
+DAYLITE_DATABASE_CONFIG = 'config/database.yml'
+DAYLITE_CONFIG_SECTION = 'daylite'
 
-if (yaml['daylite'])
-  Daylite::Base.establish_connection(yaml['daylite'])
+if File.exist?(DAYLITE_DATABASE_CONFIG)
+  yaml = YAML::load(File.read(DAYLITE_DATABASE_CONFIG))
+
+  if yaml[DAYLITE_CONFIG_SECTION]
+    Daylite::Base.establish_connection( yaml[DAYLITE_CONFIG_SECTION] )
+  end
 end
