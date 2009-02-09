3bf5aac53b99328ab42970ef410f0629f37eb859
Dmitriy Dzema <dmitriy@dzema.name>
2009-02-09 20:58:49 +1000

    rename test file

--- test/daylite_models_plugin_test.rb
+++ test/daylite_models_plugin_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UsersPluginTest < ActiveSupport::TestCase
+class DayliteModelsPluginTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
