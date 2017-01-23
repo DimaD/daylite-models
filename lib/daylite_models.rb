9c4b2c52a95037b194a67d103a68ace015ec233b
kitebuggy <jason_holloway@mac.com>
2017-01-23 19:47:22 +0000

    Autoloading fix

--- lib/daylite_models.rb
+++ lib/daylite_models.rb
@@ -5,10 +5,10 @@ require 'pg'
 models_path = File.join(File.dirname(__FILE__), 'app', 'models')
 daylite_path = File.join(models_path, 'daylite')
 
-$LOAD_PATH << models_path << daylite_path << '.'
-
-ActiveSupport::Dependencies.autoload_paths << models_path << daylite_path << '.'
+$: << models_path << daylite_path << '.'
+# $LOAD_PATH << models_path << daylite_path << '.'
+# ActiveSupport::Dependencies.autoload_paths << models_path << daylite_path << '.'
 
 module DayliteModels
-  VERSION = '0.4.0'
+  VERSION = '0.4.2'
 end
