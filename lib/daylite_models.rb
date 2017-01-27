fec8aab7ac3825979775410092bcc6463450acaa
Toby Marsden <toby@toby.org.uk>
2017-01-27 16:06:55 +0100

    Require daylite/base explicitly

--- lib/daylite_models.rb
+++ lib/daylite_models.rb
@@ -10,6 +10,7 @@ require 'daylite'
 # $LOAD_PATH << models_path << daylite_path << '.'
 # ActiveSupport::Dependencies.autoload_paths << models_path << daylite_path << '.'
 
+require 'daylite/base'
 Dir[File.join(__dir__, 'daylite', '*.rb')].each {|file| require file }
 
 module DayliteModels
