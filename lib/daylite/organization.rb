cd9aad09eb7b7821f54504364374ac5073fdb22b
kitebuggy <jason_holloway@mac.com>
2017-11-22 14:32:55 +0000

    Added details to Daylite::Organization

--- lib/daylite/organization.rb
+++ lib/daylite/organization.rb
@@ -2,6 +2,7 @@ class Daylite::Organization < Daylite::Base
   belongs_to :owner, class_name: "User", foreign_key: "assignedtoid"
   belongs_to :category, foreign_key: "categoryid"
   belongs_to :industrytype, foreign_key: "industrytypeid"
+  belongs_to :large_data, class_name: "largedata", foreign_key: "detailsid"
 
   has_many :organizationtaskjoins, foreign_key: "organizationid"
   has_many :tasks, through: :organizationtaskjoins
